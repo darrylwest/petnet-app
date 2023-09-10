cac2c30b3f694ea71dd1ce1b0ec0f519c50e6905
darryl west <darryl.west@raincitysoftware.com>
2023-09-10 15:26:18 -0700

    fixed import of pydomkeys

--- .repl-start.py
+++ .repl-start.py
@@ -1,5 +1,8 @@
 import sys
 import os
+
+from pathlib import Path
+
 import time
 from datetime import datetime
 # import importlib
@@ -8,10 +11,14 @@ from rich import inspect
 import platform
 
 # print("pid=", os.getpid())
+
+user_home = Path.home().as_posix()
+
 if platform.system() == 'Linux':
-    sys.path.append(f'{os.getenv("HOME")}/.cache/pypoetry/virtualenvs/petnet-app-c3NNDXsb-py3.11/lib/python3.11/site-packages')
+    libpath = Path(f'{user_home}/.cache/pypoetry/virtualenvs/petnet-app-c3NNDXsb-py3.11/lib/python3.11/site-packages').as_posix()
 else:
-    sys.path.append(f'{os.getenv("HOME")}/Library/Caches/pypoetry/virtualenvs/petnet-app-I8h4Bcsw-py3.11/lib/python3.11/site-packages')
+    libpath = Path(f'{user_home}/Library/Caches/pypoetry/virtualenvs/petnet-app-I8h4Bcsw-py3.11/lib/python3.11/site-packages').as_posix()
+
+sys.path.append(libpath)
 
 from tests import test_main, test_user
-

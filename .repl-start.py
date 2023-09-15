87fcf28ac640702c1d6ff0402146b43c6e09220c
darryl west <darryl.west@raincitysoftware.com>
2023-09-15 15:23:38 -0700

    fixed repl startup

--- .repl-start.py
+++ .repl-start.py
@@ -26,7 +26,8 @@ from tests import test_main, test_user, test_version, test_status, fake_data_sto
 from petnet_app.models.version import Version
 from petnet_app.models.status import Status
 from petnet_app.models.user import UserModel, Person
-from petnet_app.db.user_db import UserDb, DataStore, DataStoreConfig
+from petnet_app.db.user_db import UserDb
+from petnet_app.db.data_store import DataStore, DataStoreConfig
 
 fake = fake_data_store.FakeDataStore()
 
